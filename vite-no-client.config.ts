d7c72aa4e1f923412f5966039c8cd60a5a2ae5ae
Chris Sterritt <csterritt@gmail.com>
2025-03-19 20:19:50 -0400

    vite.config.ts set up to serve on port 3000, with no hmr.

--- vite-no-client.config.ts
+++ vite-no-client.config.ts
@@ -2,17 +2,17 @@ import build from '@hono/vite-build/cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     build(),
     devServer({
       adapter,
-      entry: 'src/index.tsx'
-    })
+      entry: 'src/index.tsx',
+      injectClientScript: false,
+    }),
   ],
-  server: {
-    port: 3000,
-    hmr: false,
-  },
+  server: { port: 3000 },
 })
